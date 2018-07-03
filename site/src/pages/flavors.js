f1cc6c96e5312fb68a79d675e71c894275764253
montoyamoraga <montoyamoraga@gmail.com>
2018-07-03 14:33:56 -0400

    add flavors and instruments links

--- site/src/pages/flavors.js
+++ site/src/pages/flavors.js
@@ -4,14 +4,14 @@ import Link from "gatsby-link";
 export default () => (
   <div>
       <h1>Flavors</h1>
-      <p>maquinitas is written using different computer music languages.</p>
-      <p>maquinitas is currently being written in:</p>
+      <p>maquinitas is currently being developed using different computer music languages.</p>
+      <p>maquinitas flavors include:</p>
       <ul>
-        <li>ChucK</li>
-        <li>Max</li>
-        <li>Max for Live</li>
-        <li>Pure Data</li>
-        <li>SuperCollider</li>
+        <li><a href="https://github.com/maquinitas/maquinitas-chuck">maquinitas-chuck</a>: written with <a href="http://chuck.cs.princeton.edu/">ChucK</a></li>
+        <li><a href="https://github.com/maquinitas/maquinitas-max">maquinitas-max</a>: written with  <a href="https://cycling74.com/products/max/">Max</a></li>
+        <li><a href="https://github.com/maquinitas/maquinitas-max">maquinitas-m4l</a>: written with <a href="https://www.ableton.com/en/live/max-for-live/">Max for Live</a></li>
+        <li><a href="https://github.com/maquinitas/maquinitas-pure-data">maquinitas-pure-data</a>:  written with <a href="http://puredata.info/">Pure Data</a></li>
+        <li><a href="https://github.com/maquinitas/maquinitas-supercollider">maquinitas-supercollider</a>: written with <a href="https://supercollider.github.io/">SuperCollider</a></li>
       </ul>
   </div>
 );
--- site/src/pages/instruments.js
+++ site/src/pages/instruments.js
@@ -8,10 +8,10 @@ export default () => (
       <p>maquinitas works with synthesizers, samplers, drum machines, sequencers, etc.</p>
       <p>Some of the instruments currently supported include:</p>
       <ul>
-        <li>BASTL microGranny 2</li>
-        <li>Korg volca fm</li>
-        <li>Moog Minitaur</li>
-        <li>Roland TR-09</li>
+        <li><a href="http://www.bastl-instruments.com/instruments/microgranny/">BASTL microGranny 2</a>: sampler</li>
+        <li><a href="https://www.korg.com/us/products/dj/volca_fm/">Korg volca fm</a>: FM synthesizer</li>
+        <li><a href="https://www.moogmusic.com/products/taurus/minitaur">Moog Minitaur</a>: bass synthesizer</li>
+        <li><a href="https://www.roland.com/global/products/tr-09/">Roland TR-09</a>: drum machine</li>
       </ul>
   </div>
 );
