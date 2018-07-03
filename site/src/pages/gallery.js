8c70e6c802e688ba9334b906ec29ef19303e16fa
montoyamoraga <montoyamoraga@gmail.com>
2018-07-03 15:38:22 -0400

    add gallery-under-construction with images

--- site/src/pages/gallery.js
+++ site/src/pages/gallery.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Link from "gatsby-link";
+import Img from 'gatsby-image';
 
-export default () => (
+export default ({ data }) => (
   <div>
       <h1>Gallery</h1>
       <p>coming soon...</p>
