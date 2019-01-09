d15dbe11e7040b99435f81cd7b11ed3cf02a349a
montoyamoraga <montoyamoraga@gmail.com>
2019-01-09 11:14:45 -0300

    add build after adding helmet

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-contact-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
   "component---src-pages-flavors-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/flavors.js" /* webpackChunkName: "component---src-pages-flavors-js" */),
   "component---src-pages-gallery-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/gallery.js" /* webpackChunkName: "component---src-pages-gallery-js" */),
