ead9eb20907796cf72d5287f91dbe99e169fb0fa
montoyamoraga <montoyamoraga@gmail.com>
2019-01-09 10:25:41 -0300

    delete unused dependencies of pages

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,11 +3,11 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-index-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-instruments-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/instruments.js" /* webpackChunkName: "component---src-pages-instruments-js" */),
-  "component---src-pages-flavors-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/flavors.js" /* webpackChunkName: "component---src-pages-flavors-js" */),
   "component---src-pages-contact-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-gallery-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/gallery.js" /* webpackChunkName: "component---src-pages-gallery-js" */)
+  "component---src-pages-flavors-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/flavors.js" /* webpackChunkName: "component---src-pages-flavors-js" */),
+  "component---src-pages-gallery-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/gallery.js" /* webpackChunkName: "component---src-pages-gallery-js" */),
+  "component---src-pages-index-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-instruments-js": () => import("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/instruments.js" /* webpackChunkName: "component---src-pages-instruments-js" */)
 }
 
 exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/.cache/data.json")
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,10 +6,10 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/.cache/dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/index.js"))),
-  "component---src-pages-instruments-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/instruments.js"))),
-  "component---src-pages-flavors-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/flavors.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/contact.js"))),
-  "component---src-pages-gallery-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/gallery.js")))
+  "component---src-pages-flavors-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/flavors.js"))),
+  "component---src-pages-gallery-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/gallery.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/index.js"))),
+  "component---src-pages-instruments-js": hot(preferDefault(require("/Users/montoyamoraga/github/maquinitas/maquinitas.github.io/src/pages/instruments.js")))
 }
 
